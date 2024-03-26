5af9aeffba83f7cfd04831592f059a223252e800
salahahmed2001 <salah01068441@gmail.com>
2024-03-26 07:43:56 +0200

    add new files and html and style css and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,19 +35,96 @@ window.addEventListener("scroll", function () {
       deliveryBoyMove -= 1;
     }
     lastScrollPos = activeScrollPos;
-    DeliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`
+    DeliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
   }
 });
 
-// window.addEventListener("scroll", function () {
-//   let value = scrollY;
-//   if (this.scrollY >= 1900) {
-//     // DeliveryBoy.style.left = value + "px";
-//     DeliveryBoy.style.left = "100px";
-//   } else {
-//     DeliveryBoy.style.left = "0px";
-//     // DeliveryBoy.style.right = value + "px";
-//   }
-// });
+let BgLoader = document.querySelector(".bg-loader");
+setTimeout(function () {
+  BgLoader.style.display = "none";
+}, 1000);
 
-// data-delivery-boy
+async function getProducts() {
+  let data = await fetch("http://127.0.0.1:5501/products.json")
+    .then((el) => el.json())
+    .then((ele) => {
+      return ele;
+    });
+  let MyData = ``;
+  data.map((el) => {
+    MyData += `
+    
+    <div class="card">
+    <div class="red">${el.discount}</div>
+    <div class="img-card">
+      <button class="btn2">Order now</button>
+      <img src="${el.image}" alt="" />
+    </div>
+
+    <div class="text-card">
+      <div class="reting">
+        <p class="r-p">${el.type}</p>
+        <span class="ico">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </span>
+      </div>
+      <h3>${el.name}</h3>
+
+      <h5>
+        <span class="price">PRICE:</span>
+        <span class="sp1">${el.discountedPris}</span><span class="smo">${el.price}</span>
+      </h5>
+    </div>
+  </div>
+
+    `;
+  });
+  document.querySelector(".menu-card-row").innerHTML = MyData;
+}
+getProducts();
+
+async function getProducts2() {
+  let data = await fetch("http://127.0.0.1:5501/products2.json")
+    .then((el) => el.json())
+    .then((ele) => {
+      return ele;
+    });
+  let MyData = ``;
+  data.map((el) => {
+    MyData += `
+    <div class="card">
+    <div class="red">${el.discount}</div>
+    <div class="img-card">
+      <button class="btn2">Order now</button>
+      <img src="${el.image}" alt="" />
+    </div>
+
+    <div class="text-card">
+      <div class="reting">
+        <p class="r-p">${el.type}</p>
+        <span class="ico">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </span>
+      </div>
+      <h3>${el.name}</h3>
+
+      <h5>
+        <span class="price">PRICE:</span>
+        <span class="sp1">${el.discountedPris}</span><span class="smo">${el.price}</span>
+      </h5>
+    </div>
+  </div>
+  
+    `;
+  });
+  document.querySelector(".menue-card-row2").innerHTML = MyData;
+}
+getProducts2();
